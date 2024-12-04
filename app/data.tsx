38be5af3dc347b9cb02aedd6388c09a0e2364396
Maricel <montuyamaricela@gmail.com>
2024-12-05 02:25:46 +0800

    update data

--- app/data.tsx
+++ app/data.tsx
@@ -34,20 +34,20 @@ export const AppFeatures = [
     description: "Quickly alert for help when it matters most.",
     icon: EmergencyIcon.src,
   },
-  {
-    id: 3,
-    title: "Lorem Ipsum",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur interdum",
-    icon: MapIcon.src,
-  },
-  {
-    id: 4,
-    title: "Lorem Ipsum",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur interdum",
-    icon: MapIcon.src,
-  },
+  // {
+  //   id: 3,
+  //   title: "Jeepney Route Assistance",
+  //   description:
+  //     "Easily locate jeepney routes, stops, and fares for more efficient commuting.",
+  //   icon: MapIcon.src,
+  // },
+  // {
+  //   id: 4,
+  //   title: "Lorem Ipsum",
+  //   description:
+  //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur interdum",
+  //   icon: MapIcon.src,
+  // },
 ];
 
 export const TeamProfile = [
